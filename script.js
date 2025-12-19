63c9c16c47f46be89b9fa4a98e0d7e91b7d04102
Erick Silva <donizttierick@gmail.com>
2025-12-19 15:12:13 -0300

    .

---
+++ script.js
@@ -0,0 +1,10 @@
+// Array para armazenar os dados
+let armazenarDados = []
+
+// Indice para percorrer o array
+indice = 0
+
+// Sidas
+let estatisca = document.getElementById('estatisca')
+let res = document.getElementById('lista')
+
