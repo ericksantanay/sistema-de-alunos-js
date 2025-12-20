aae0628a7b0b59da7f8fc518360aa513c7874a60
Erick Silva <donizttierick@gmail.com>
2025-12-20 09:31:03 -0300

    Ajustes

--- script.js
+++ script.js
@@ -22,17 +22,39 @@ let reprovados = 0
 // Recuperação
 let recuperação
 
-// Sidas
-let estatisca = document.getElementById('estatisca')
+
+// Saidas
 let res = document.getElementById('lista')
+
+
+// Saida das estatistica
+// Saida da media
+let me = document.getElementById('media')
+
+// Saida da maior nota
+let maiorN = document.getElementById('maiorNota')
+
+// Saida da menor Nota
+let menorN = document.getElementById('menorNota')
+
+// Saida dos aprovados
+let ap = document.getAnimations('aprovados')
+
+// Saida dos Reprovados
+let rep = document.getElementById('reprovados')
+
+// Saida dos que estão em Recuperação
+let rec = document.getElementById('recuperacao')
+
 
 // Função adicionar
 function Adicionar() {
     let nome = document.getElementById('nome').value
-    let nota = document.getElementById('nota').value
+    let nota = Number(document.getElementById('nota').value)
 
-    if (nome === '' && nota === '') {
+    if (nome === '') {
         alert('Preencha os Campos!')
+        return
     }
 
     // Colocando tudo em um objeto
@@ -52,13 +74,19 @@ function Adicionar() {
 
 // Função Para Não repetir codigo
 function atualizarLista() {
-    //Server para não repetir as lista
-    estatisca.innerHTML = ''
+    //Server para não repetir 
     res.innerHTML = ''
+    media = 0 // zerarando a soma antes do forEach. // Reset da soma!!!
+    maiorNota = 0
+    menorNota = 0 
+    aprovados = 0
+    reprovados = 0
+    recuperação = 0
 
 
     // Laço que vai percorrer o array 
     armazenarDados.forEach((item, indice) => {
+        
         res.innerHTML += 
         `
             <div class="alunos">
@@ -67,14 +95,13 @@ function atualizarLista() {
                     <button onclick="Remover(${indice})">Remover</button>
             </div>
         `
-        media += item.nota
+        media += item.nota // Acumulando o valor
     });
-
-    
-
-
-
 }
+
+
+
+
 
 // Função de remover o aluno
 function Remover(indice) {
@@ -84,9 +111,14 @@ function Remover(indice) {
 }
 
 
+
+
 // Função da estatistica
 function CalcularResultado() {
+    
+    me.innerText = `Média geral da turma: ${media}`
    
+
 
     atualizarLista()
 }
